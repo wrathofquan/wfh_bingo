d12dca1195cdc425e3c5ab410ed3722617938eda
Josh Quan <wrathofquan@gmail.com>
2020-03-19 16:28:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Working From Home During a Global Pandemic Bingo
+##### by @wrathofquan, inspired by [Kimberly Harington](https://www.mcsweeneys.net/articles/working-from-home-during-a-global-pandemic-bingo)
+---
+
---
+++ js/data.js
@@ -0,0 +1,42 @@
+
+var headerText = "";
+
+var footerText = "";
+
+var winText = "Bingo! You win!";
+
+var clickSnd = new Audio("");
+
+var winSnd = new Audio("");
+
+
+
+var JSONBingo = {"squares": [
+        {"square": "Put pants on before dialing in"},
+        {"square": "Time losing all meaning"},
+        {"square": "Wish you had been calling it 'social distancing' all along"},
+        {"square": "Contagion jokes"},
+        {"square": "Family time now quantity over quality"},
+        {"square": "Existential dread"},
+        {"square": "Mornings suddenly enjoyable and full of possibility"},
+        {"square": "Afraid of own hands"},
+        {"square": "'Everything is fine' meme"},
+        {"square": "Cadbury Mini Eggs for breakfast"},
+        {"square": "Meetings losing all meaning"},
+        {"square": "Unnatural longing for office birthday cake"},
+        {"square": "Sudden everyone-is-talking-at-the-same-time-why"},
+        {"square": "Pet time now constant and necessary"},
+        {"square": "Full screen freeze with mouth wide open"},
+        {"square": "Practical dread"},
+        {"square": "Laugh to keep from freaking the fuck out"},
+        {"square": "Your grandma was right, going for a walk is nice"},
+        {"square": "Seeing co-workers' bedrooms"},
+        {"square": "Only hear every fifth word but get the gist"},
+        {"square": "Me time now incredibly too much"},
+        {"square": "Meaning losing all meaning"},
+        {"square": "Full screen freeze but sound keeps going as if it exists in a parallel universe"},
+        {"square": "Pound of bacon for lunch"},
+        {"square": "Pet walks into frame"},
+        {"square": "Refreshing New York Times every minute to see if world has ended"}        
+    ]
+};
